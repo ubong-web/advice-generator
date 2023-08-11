bfb0873276df1f18fccd4c79453c8091e69a42a8
John Ubong <ubongj368@gmail.com>
2023-08-11 08:54:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const containerEl = document.querySelector(".container");
+console.log(containerEl);
+const infoEl = document.querySelector("#info");
+console.log(infoEl);
+const buttonEl = document.querySelector(".button");
+const adviceUrl = "https://api.adviceslip.com/advice";
+
+buttonEl.addEventListener("click", () => {
+  fetch(adviceUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".id-number").textContent = `${data.slip.id}`;
+      document.querySelector(".advice").textContent = `${data.slip.advice}`;
+    });
+});
